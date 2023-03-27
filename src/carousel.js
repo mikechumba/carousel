421539c4ccc705b7b514c7ac742f8b26af081dd9
Mike Chumba <michaelchumba09@gmail.com>
2023-03-27 12:14:59 +0300

    add: main button events

--- src/carousel.js
+++ src/carousel.js
@@ -15,7 +15,6 @@ const renderCarousel = (images) => {
     container.appendChild(slide)
   });
 
-
   renderImageSelectors((images || []).length)
 }
 
@@ -27,8 +26,38 @@ const renderImageSelectors = (count) => {
     if (i === activeSlide) {
       selector.setAttribute('class', 'active')
     }
+    selector.addEventListener('click', (e) => changeSlide(e.currentTarget.dataset.index));
     container.appendChild(selector);
   }
+
+  navButtonInit(count);
+}
+
+const navButtonInit = (count) => {
+  const nextBtn = document.querySelector('.main-btn.next');
+  const previousBtn = document.querySelector('.main-btn.previous');
+  const handleClick = (isNext) => {
+    const nextSlide = activeSlide + 1 === count ? 0 : activeSlide + 1;
+    const previousSlide = activeSlide === 0 ? count - 1 : activeSlide - 1;
+    isNext ? changeSlide(nextSlide) : changeSlide(previousSlide)
+  }
+  nextBtn.addEventListener('click', () => handleClick(true));
+  previousBtn.addEventListener('click', () => handleClick(false));
+}
+
+
+const changeSlide = (index) => {
+  const container = document.querySelector('.carousel');
+
+  container.childNodes[activeSlide].setAttribute('class', 'slide')
+  container.childNodes[index].setAttribute('class', 'slide active');
+
+  const selectors = document.querySelector('.selectors');
+
+  selectors.childNodes[activeSlide].setAttribute('class', '')
+  selectors.childNodes[index].setAttribute('class', 'active');
+
+  activeSlide = index;
 }
 
 window.onload = async () => {
