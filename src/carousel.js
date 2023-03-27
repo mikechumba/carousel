7997c68d643a52ec3171fcabd1e198ae79de1318
Mike Chumba <michaelchumba09@gmail.com>
2023-03-27 10:02:38 +0300

    add: carousel component to render fetched images

--- src/carousel.js
+++ src/carousel.js
@@ -1,3 +1,21 @@
 import { fetchData } from "./api";
 
-fetchData();
+const renderCarousel = (images) => {
+  const container = document.querySelector('.carousel');
+
+  images.forEach(({ src, alt }) => {
+    const slide = document.createElement('div');
+    slide.setAttribute('class', 'slide');
+    const img = document.createElement('img');
+    img.src = src;
+    img.alt = alt;
+    slide.appendChild(img)
+    container.appendChild(slide)
+  })
+
+}
+
+window.onload = async () => {
+  const images = await fetchData();
+  renderCarousel(images);
+}
