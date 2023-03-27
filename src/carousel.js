9664443582ceebb072dfc842aa33d341b7061369
Mike Chumba <michaelchumba09@gmail.com>
2023-03-27 10:53:48 +0300

    add: main nav buttons

--- src/carousel.js
+++ src/carousel.js
@@ -1,17 +1,19 @@
 import { fetchData } from "./api";
 
+let activeSlide = 0;
+
 const renderCarousel = (images) => {
   const container = document.querySelector('.carousel');
 
-  images.forEach(({ src, alt }) => {
+  images.forEach(({ src, alt }, i) => {
     const slide = document.createElement('div');
-    slide.setAttribute('class', 'slide');
+    slide.setAttribute('class', 'slide' + `${i === activeSlide ? ' active' : ''}`);
     const img = document.createElement('img');
     img.src = src;
     img.alt = alt;
     slide.appendChild(img)
     container.appendChild(slide)
-  })
+  });
 
 }
 
